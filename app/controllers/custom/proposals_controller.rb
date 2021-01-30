df3f9992c7d5912a92adde02362337a271efa051
itw-smart <tobias@demokratie.today>
2021-01-30 22:32:05 +0100

    or instead of and

--- app/controllers/custom/proposals_controller.rb
+++ app/controllers/custom/proposals_controller.rb
@@ -14,7 +14,7 @@ class ProposalsController
   private
     def take_only_by_tag_names
       if params[:tags].present?
-        @resources = @resources.tagged_with(params[:tags].split(","), all: true)
+        @resources = @resources.tagged_with(params[:tags].split(","), all: true, any: :true)
         @subcategories = @resources.tag_counts.subcategory
       end
     end
